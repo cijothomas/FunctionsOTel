8ee00f746d3f8e443c93baf9e9fc245c5f7f85f9
Cijo Thomas <cithomas@microsoft.com>
2021-12-17 14:08:17 -0800

    Add project files.

---
+++ FunctionsOTel/Startup.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Logs;
+
+[assembly: FunctionsStartup(typeof(FunctionsOTel.Startup))]
+namespace FunctionsOTel
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddLogging((loggingBuilder) =>
+            {
+                // Configure logging filter here in code
+                // or in host.json
+                // loggingBuilder.AddFilter<OpenTelemetryLoggerProvider>("*", LogLevel.Warning);
+                loggingBuilder.AddOpenTelemetry((otelOptions) => otelOptions.AddConsoleExporter());
+            }
+            );
+        }
+    }
+}
